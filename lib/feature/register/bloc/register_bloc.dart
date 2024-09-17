90d971e8adbe4b5ee39295a119b8342e32e7b2a4
Srujankm12 <srujankm12@gmail.com>
2024-09-17 00:04:30 -0700

    no issue

--- lib/feature/register/bloc/register_bloc.dart
+++ lib/feature/register/bloc/register_bloc.dart
@@ -28,7 +28,11 @@ class RegisterBloc extends Bloc<RegisterEvent, RegisterState> {
         );
         var response = jsonDecode(jsonResponse.body);
         if (jsonResponse.statusCode == 200) {
-          emit(FetchMachinesSuccessState(data: response['data']));
+          List<String> unitId = [];
+          for (int i = 0; i < response['data'].length; i++) {
+            unitId.add(response['data'][i]['unit_id']);
+          }
+          emit(FetchMachinesSuccessState(data: unitId));
           return;
         }
         emit(RegisterFailureState(err: response['message']));
@@ -39,27 +43,21 @@ class RegisterBloc extends Bloc<RegisterEvent, RegisterState> {
     on<FetchStudentUnitIdEvent>((event, emit) async {
       try {
         emit(RegisterLoadingState());
-        var jsonResponse = await http.post(Uri.parse(HttpRoutes.fetchStudents) , body: jsonEncode({
-          "unit_id": event.unitID,
-        },),);
+        var jsonResponse = await http.post(
+          Uri.parse(HttpRoutes.fetchStudents),
+          body: jsonEncode({"unit_id": event.unitID}),
+        );
         var response = jsonDecode(jsonResponse.body);
-        if(jsonResponse.statusCode == 200){
-          List<String> studentUnitID = [];
-          int same = 0;
-          for(int i = 0 ; i < response['data'].length ; i++){
-            for(int j = 0 ; j <= 256 ; j++){
-              if(int.parse(response['data'][i]['student_unit_id']) == j){
-                same = j;
-                break;
-              }
-            }
-            if(same == 0){
-              studentUnitID.add(response['data'][i]['student_unit_id']);
-              same = 0;
-            }
-            same = 0;
+        if (jsonResponse.statusCode == 200) {
+          Set<int> allNumbers = Set<int>.from(List.generate(257, (i) => i));
+          for (var item in response['data']) {
+            int id = int.parse(item['student_unit_id']);
+            allNumbers.remove(id);
           }
-          emit(FetchStudentUnitIdSuccessState(data: studentUnitID)); 
+          List<String> studentUnitID =
+              allNumbers.map((id) => id.toString()).toList();
+          emit(FetchStudentUnitIdSuccessState(data: studentUnitID));
+
           return;
         }
         emit(RegisterFailureState(err: response['message']));
@@ -67,10 +65,12 @@ class RegisterBloc extends Bloc<RegisterEvent, RegisterState> {
         emit(RegisterFailureState(err: e.toString()));
       }
     });
-    on<FetchComPortsEvent>((event , emit) async {
+
+    on<FetchComPortsEvent>((event, emit) async {
       try {
         List<String> serialPorts = SerialPort.availablePorts;
-        if(serialPorts.isEmpty){
+        print(serialPorts);
+        if (serialPorts.isEmpty) {
           emit(RegisterFailureState(err: "No Ports Found..."));
         }
         emit(FetchAllPortsSuccessState(data: serialPorts));
@@ -78,36 +78,36 @@ class RegisterBloc extends Bloc<RegisterEvent, RegisterState> {
         emit(RegisterFailureState(err: e.toString()));
       }
     });
-    on<RegisterStudentEvent>((event , emit) async {
-      try{
+    on<RegisterStudentEvent>((event, emit) async {
+      try {
         emit(RegisterLoadingState());
         var port = SerialPort(event.port);
         String fingerprintData = "";
-        if(!port.openReadWrite()){
+        if (!port.openReadWrite()) {
           emit(RegisterFailureState(err: "Unable to open port..."));
           return;
         }
         final reader = SerialPortReader(port);
-        reader.stream.listen((data){
+        reader.stream.listen((data) {
           fingerprintData = utf8.decode(data);
         });
-        var jsonReponse = await http.post(Uri.parse(HttpRoutes.registerStudent),body: jsonEncode(
-          {
-            "student_unit_id": event.studentUnitId,
-            "unit_id": event.unitID,
-            "student_name": event.studentName,
-            "student_usn": event.studentUSN,
-            "department": event.studentDepartment,
-            "fingerprint": fingerprintData,
-          }
-        ));
+        var jsonReponse = await http.post(Uri.parse(HttpRoutes.registerStudent),
+            body: jsonEncode({
+              "student_unit_id": event.studentUnitId,
+              "unit_id": event.unitID,
+              "student_name": event.studentName,
+              "student_usn": event.studentUSN,
+              "department": event.studentDepartment,
+              "fingerprint": fingerprintData,
+            }));
         var response = jsonDecode(jsonReponse.body);
-        if(jsonReponse.statusCode == 200){
+        print(response);
+        if (jsonReponse.statusCode == 200) {
           emit(RegisterSuccessState(message: response['message']));
           return;
         }
         emit(RegisterFailureState(err: response['message']));
-      }catch(e){
+      } catch (e) {
         emit(RegisterFailureState(err: e.toString()));
       }
     });
--- lib/feature/register/pages/register_student_page.dart
+++ lib/feature/register/pages/register_student_page.dart
@@ -16,161 +16,244 @@ class RegisterPage extends StatefulWidget {
 
 class _RegisterPageState extends State<RegisterPage> {
   final TextEditingController _nameController = TextEditingController();
-  String unitId = "";
-  String studentUnitId = "";
-  String ports = "";
+  final TextEditingController _usnController = TextEditingController();
+  final TextEditingController _branchController = TextEditingController();
+  String unitId = "Select the unit";
+  String studentUnitId = "Select any Number";
+  String ports = "Select the Register Port";
+
+  @override
+  void initState() {
+    BlocProvider.of<RegisterBloc>(context).add(
+      FetchMachinesEvent(),
+    );
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.grey.shade900,
-      appBar: AppBar(
-        title: Text(
-          "Register Student",
-          style: GoogleFonts.nunito(
-              color: Colors.white, fontSize: 20, fontWeight: FontWeight.bold),
-        ),
+    return BlocListener<RegisterBloc, RegisterState>(
+      listener: (context, state) {
+        if (state is RegisterFailureState) {
+          ScaffoldMessenger.of(context).showSnackBar(
+            SnackBar(
+              content: Text(state.err),
+            ),
+          );
+        }
+      },
+      child: Scaffold(
         backgroundColor: Colors.grey.shade900,
-        iconTheme: const IconThemeData(
-          color: Colors.white,
-          size: 30,
-        ),
-      ),
-      body: Container(
-        width: double.infinity,
-        height: double.infinity,
-        decoration: BoxDecoration(
-          border: Border.all(color: Colors.black),
-          color: Colors.white,
-          borderRadius: const BorderRadius.only(
-            topLeft: Radius.circular(20),
-            topRight: Radius.circular(20),
+        appBar: AppBar(
+          title: Text(
+            "Register Student",
+            style: GoogleFonts.nunito(
+                color: Colors.white, fontSize: 20, fontWeight: FontWeight.bold),
+          ),
+          backgroundColor: Colors.grey.shade900,
+          iconTheme: const IconThemeData(
+            color: Colors.white,
+            size: 30,
           ),
         ),
-        child: Center(
-          child: SizedBox(
-            width: 500,
-            height: 600,
-            child: Card(
-              elevation: 8,
-              color: Colors.white,
-              child: Padding(
-                padding: const EdgeInsets.all(16.0),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: [
-                    const Icon(
-                      Icons.fingerprint,
-                      size: 100,
-                    ),
-                    CustomTextField(
-                      prefixIcon: Icons.person,
-                      hintText: "Name",
-                      isObscure: false,
-                      controller: _nameController,
-                      isPasswordField: false,
-                    ),
-                    CustomTextField(
-                      prefixIcon: Icons.numbers,
-                      hintText: "USN",
-                      isObscure: false,
-                      controller: _nameController,
-                      isPasswordField: false,
-                    ),
-                    CustomTextField(
-                      prefixIcon: Icons.book,
-                      hintText: "Department",
-                      isObscure: false,
-                      controller: _nameController,
-                      isPasswordField: false,
-                    ),
-                    BlocBuilder<RegisterBloc, RegisterState>(
-                      builder: (context, state) {
-                        if (state is FetchMachinesSuccessState) {
-                          return CustomDropDownMenu(
-                            data: state.data,
-                            onChanged: (p0) {
-                              unitId = p0!;
-                            },
-                          );
-                        }
-                        return Container(
-                          width: 400,
-                          height: 40,
-                          decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(10),
-                            color: Colors.grey.shade200,
-                          ),
-                          child: Text(unitId),
-                        );
-                      },
-                    ),
-                    BlocBuilder<RegisterBloc, RegisterState>(
-                      builder: (context, state) {
-                        if (state is FetchStudentUnitIdSuccessState) {
-                          return CustomDropDownMenu(
-                            data: state.data,
-                            onChanged: (p0) {
-                              studentUnitId = p0!;
-                            },
-                          );
-                        }
-                        return Container(
-                          width: 400,
-                          height: 40,
-                          decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(10),
-                            color: Colors.grey.shade200,
-                          ),
-                          child: Text(unitId),
-                        );
-                      },
-                    ),
-                    BlocBuilder<RegisterBloc, RegisterState>(
-                      builder: (context, state) {
-                        if(state is FetchAllPortsSuccessState){
-                          return CustomDropDownMenu(
-                          data: state.data,
-                          onChanged: (p0) {
-                            ports = p0!;
-                          },
-                        );
-                        }
-                        return Container(
-                          width: 400,
-                          height: 40,
-                          decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(10),
-                            color: Colors.grey.shade200,
-                          ),
-                          child: Text(unitId),
-                        );
-                      },
-                    ),
-                    SizedBox(
-                      width: double.infinity,
-                      height: 44,
-                      child: ElevatedButton(
-                        onPressed: () {
-                          setState(() {
-                            CustomFingerprintDialog.dialog(context);
-                          });
-                        },
-                        style: ElevatedButton.styleFrom(
-                          backgroundColor: Colors.black,
-                          shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(10)),
-                        ),
-                        child: Text(
-                          "Take Fingerprint",
-                          style: GoogleFonts.nunito(
-                            color: AppColors.whiteColor,
-                            fontSize: 22,
-                            fontWeight: FontWeight.bold,
-                          ),
-                        ),
+        body: Container(
+          width: double.infinity,
+          height: double.infinity,
+          decoration: BoxDecoration(
+            border: Border.all(color: Colors.black),
+            color: Colors.white,
+            borderRadius: const BorderRadius.only(
+              topLeft: Radius.circular(20),
+              topRight: Radius.circular(20),
+            ),
+          ),
+          child: Center(
+            child: SizedBox(
+              width: 500,
+              height: 600,
+              child: Card(
+                elevation: 8,
+                color: Colors.white,
+                child: Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      const Icon(
+                        Icons.fingerprint,
+                        size: 100,
                       ),
-                    ),
-                  ],
+                      CustomTextField(
+                        prefixIcon: Icons.person,
+                        hintText: "Name",
+                        isObscure: false,
+                        controller: _nameController,
+                        isPasswordField: false,
+                      ),
+                      CustomTextField(
+                        prefixIcon: Icons.numbers,
+                        hintText: "USN",
+                        isObscure: false,
+                        controller: _usnController,
+                        isPasswordField: false,
+                      ),
+                      CustomTextField(
+                        prefixIcon: Icons.book,
+                        hintText: "Department",
+                        isObscure: false,
+                        controller: _branchController,
+                        isPasswordField: false,
+                      ),
+                      BlocBuilder<RegisterBloc, RegisterState>(
+                        builder: (context, state) {
+                          if (state is FetchMachinesSuccessState) {
+                            return CustomDropDownMenu(
+                              data: state.data,
+                              onChanged: (p0) {
+                                unitId = p0!;
+                                BlocProvider.of<RegisterBloc>(context).add(
+                                  FetchStudentUnitIdEvent(unitID: unitId),
+                                );
+                              },
+                            );
+                          }
+                          return Container(
+                            width: 500,
+                            height: 44,
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(10),
+                              color: Colors.grey.shade200,
+                            ),
+                            child: Center(
+                                child: Text(
+                              unitId,
+                              style: GoogleFonts.nunito(
+                                  color: Colors.grey,
+                                  fontSize: 16,
+                                  fontWeight: FontWeight.w500),
+                            )),
+                          );
+                        },
+                      ),
+                      BlocBuilder<RegisterBloc, RegisterState>(
+                        builder: (context, state) {
+                          if (state is FetchStudentUnitIdSuccessState) {
+                            return CustomDropDownMenu(
+                              data: state.data,
+                              onChanged: (p0) {
+                                studentUnitId = p0!;
+                                BlocProvider.of<RegisterBloc>(context).add(
+                                  FetchComPortsEvent(),
+                                );
+                              },
+                            );
+                          }
+                          return Container(
+                            width: 500,
+                            height: 44,
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(10),
+                              color: Colors.grey.shade200,
+                            ),
+                            child: Center(
+                                child: Text(
+                              studentUnitId,
+                              style: GoogleFonts.nunito(
+                                  color: Colors.grey,
+                                  fontSize: 16,
+                                  fontWeight: FontWeight.w500),
+                            )),
+                          );
+                        },
+                      ),
+                      BlocBuilder<RegisterBloc, RegisterState>(
+                        builder: (context, state) {
+                          if (state is FetchAllPortsSuccessState) {
+                            return CustomDropDownMenu(
+                              data: state.data,
+                              onChanged: (p0) {
+                                ports = p0!;
+                              },
+                            );
+                          }
+                          return Container(
+                            width: 500,
+                            height: 44,
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(10),
+                              color: Colors.grey.shade200,
+                            ),
+                            child: Center(
+                                child: Text(
+                              ports,
+                              style: GoogleFonts.nunito(
+                                  color: Colors.grey,
+                                  fontSize: 16,
+                                  fontWeight: FontWeight.w500),
+                            )),
+                          );
+                        },
+                      ),
+                      BlocBuilder<RegisterBloc, RegisterState>(
+                        builder: (context, state) {
+                          if (state is FetchAllPortsSuccessState) {
+                            return SizedBox(
+                              width: double.infinity,
+                              height: 44,
+                              child: ElevatedButton(
+                                onPressed: () {
+                                  setState(() {
+                                    CustomFingerprintDialog.dialog(context);
+                                  });
+                                  BlocProvider.of<RegisterBloc>(context).add(
+                                    RegisterStudentEvent(
+                                      studentName: _nameController.text,
+                                      studentUSN: _usnController.text,
+                                      studentDepartment: _branchController.text,
+                                      studentUnitId: studentUnitId,
+                                      unitID: unitId,
+                                      fingerprint: "",
+                                      port: ports,
+                                    ),  
+                                  );
+                                },
+                                style: ElevatedButton.styleFrom(
+                                  backgroundColor: Colors.black,
+                                  shape: RoundedRectangleBorder(
+                                      borderRadius: BorderRadius.circular(10)),
+                                ),
+                                child: Text(
+                                  "Take Fingerprint",
+                                  style: GoogleFonts.nunito(
+                                    color: AppColors.whiteColor,
+                                    fontSize: 22,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                              ),
+                            );
+                          }
+                          return Container(
+                            width: double.infinity,
+                            height: 44,
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(10),
+                              color: Colors.grey.shade500,
+                            ),
+                            child: Center(
+                              child: Text(
+                                "Take Fingerprint",
+                                style: GoogleFonts.nunito(
+                                    color: Colors.grey.shade100,
+                                    fontSize: 16,
+                                    fontWeight: FontWeight.w500),
+                              ),
+                            ),
+                          );
+                        },
+                      ),
+                    ],
+                  ),
                 ),
               ),
             ),
